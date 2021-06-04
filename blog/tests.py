1f5419c9a4c12eca814d1666164bd9abb798a0ff
Macachor, Jerald Louie (Contractor) <JeraldLouie_Macachor@comcast.com>
2021-06-03 19:41:32 -0700

    update, and delete view

--- blog/tests.py
+++ blog/tests.py
@@ -22,6 +22,9 @@ class BlogTest(TestCase):
     post = Post(title="A Title")
     self.assertEqual(str(post), post.title)
 
+  def test_get_absolute_url(self):
+    self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
   def test_post_content(self):
     self.assertEqual(f"{self.post.title}", "A Title")
     self.assertEqual(f"{self.post.body}", "A Body")
@@ -40,3 +43,24 @@ class BlogTest(TestCase):
     self.assertEqual(no_response.status_code, 404)
     self.assertContains(response, 'A Title')
     self.assertTemplateUsed(response, 'post_detail.html')
+
+  def test_post_create_view(self):
+    response = self.client.post(reverse('post_new'), {
+      'title': "New Title",
+      'body': 'New Body',
+      'author': self.user.id
+    })
+    self.assertEquals(response.status_code, 302)
+    self.assertEqual(Post.objects.last().title, "New Title")
+    self.assertEqual(Post.objects.last().body, "New Body")
+
+  # def test_post_update_view(self):
+  #   response = self.client.post(reverse('post_edit'), args='1', {
+  #     'title': 'Update Title',
+  #     'body' : 'Updated Body'
+  #   })
+  #   self.assertEqual(response.status_code, 302)
+
+  # def test_post_delete_view(self):
+  #   response = self.client.post(reverse('post_delete'), args='1')
+  #   self.assertEqual(response.status_code, 302)
